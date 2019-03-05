9f08bb3b99264f78a037346c823ed81063ff8c45
Fajar Septian <fajarajay10@gmail.com>
2019-03-05 15:51:10 +0700

    add library stetho

---
+++ BolaApp/app/src/main/java/com/fajarazay/github/bolaapp/MyApplication.java
@@ -0,0 +1,18 @@
+package com.fajarazay.github.bolaapp;
+
+import android.app.Application;
+
+/**
+ * Created by Fajar Septian on 2019-03-05.
+ *
+ * @Author Fajar Septian
+ * @Email fajarajay10@gmail.com
+ * @Github https://github.com/fajarazay
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+    }
+}
